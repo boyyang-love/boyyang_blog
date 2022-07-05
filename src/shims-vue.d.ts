c0adf999b13804130cc5db621c46db15df05c0ac
yang <boyyang-love@outlook.com>
2022-07-05 12:22:26 +0800

    修改请求接口

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,16 +1,15 @@
 /**
  * @Author: boyyang
  * @Date: 2022-04-04 23:54:24
- * @LastEditTime: 2022-07-04 17:32:18
+ * @LastEditTime: 2022-07-05 08:57:04
  * @LastEditors: boyyang
  * @Description:
- * @FilePath: \blog\web\src\shime-vue.d.ts
+ * @FilePath: \blog\web\src\shims-vue.d.ts
  * @[如果痛恨所处的黑暗，请你成为你想要的光。 --塞尔维亚的天空]
  */
 
 import { MessageApi, DialogApi, NotificationApi } from 'naive-ui'
 
-declare module 'qs'
 declare global {
     interface Window {
         $message: MessageApi
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,12 +1,13 @@
 /**
  * @Author: boyyang
  * @Date: 2022-07-04 17:32:09
- * @LastEditTime: 2022-07-04 17:32:09
+ * @LastEditTime: 2022-07-05 08:57:23
  * @LastEditors: boyyang
  * @Description:
  * @FilePath: \blog\web\src\shims.d.ts
  * @[如果痛恨所处的黑暗，请你成为你想要的光。 --塞尔维亚的天空]
  */
 
+declare module 'qs'
 declare module '@kangc/v-md-editor'
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js'

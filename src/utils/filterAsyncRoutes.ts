0bb98e9ea5e301ec24eb903c2fc6a6a51e6c50fd
yang <boyyang-love@outlook.com>
2022-08-08 13:51:45 +0800

    修改方法

--- src/utils/filterAsyncRoutes.ts
+++ src/utils/filterAsyncRoutes.ts
@@ -1,19 +1,19 @@
 /**
  * @Author: boyyang
  * @Date: 2022-04-08 21:56:47
- * @LastEditTime: 2022-04-09 14:23:46
+ * @LastEditTime: 2022-07-21 09:01:42
  * @LastEditors: boyyang
  * @Description:  异步路由
- * @FilePath: \drawingBed\src\utils\filterAsyncRoutes.ts
+ * @FilePath: \blog\web\src\utils\filterAsyncRoutes.ts
  * @[如果痛恨所处的黑暗，请你成为你想要的光。 --塞尔维亚的天空]
  */
 
 import { RouteRecordRaw } from 'vue-router'
 
-const hasPermission = (route: RouteRecordRaw, roles: string[],): boolean => {
-    const { meta } = route as any
+const hasPermission = <T = string>(route: RouteRecordRaw, roles: string[]): boolean => {
+    const { meta } = route || {}
     if (meta && meta.roles) {
-        return roles.some(role => meta.roles.includes(role))
+        return roles.some(role => (meta.roles as Array<T>).includes(role as unknown as T))
     } else {
         return true
     }
@@ -33,6 +33,4 @@ const filterAsyncRoutes = (routes: RouteRecordRaw[], roles: string[]): RouteReco
     return res
 }
 
-export {
-    filterAsyncRoutes
-}
+export { filterAsyncRoutes }

bab31a70031f019240509f6815aa7c0d9ff83dfc
yang <boyyang-love@outlook.com>
2023-05-18 16:42:05 +0800

    升级vue版本

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,10 +1,26 @@
 <script lang="ts" setup>
+import {Props} from './Props'
+
+const Props = withDefaults(defineProps<Props>(), {
+    name: 'boyyang'
+})
+
+console.log(Props.name)
 </script>
 
 <template>
     <div class="container">
-        <n-input type="s" allow-input="jj"></n-input>
+        <div class="name">
+            <div class="age"></div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style lang="less" scoped>
+.container {
+  .name {
+    .age {
+    }
+  }
+}
+</style>
